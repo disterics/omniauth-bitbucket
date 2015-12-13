1d87add0b549a59b10aa43bd9767c677a427abe6
birmacher <birmacher@gmail.com>
2015-12-13 14:47:24 +0100

    user info & emails

--- lib/omniauth/strategies/bitbucket2.rb
+++ lib/omniauth/strategies/bitbucket2.rb
@@ -14,6 +14,10 @@ module OmniAuth
         :token_url => 'https://bitbucket.org/site/oauth2/access_token'
       }
 
+      def request_phase
+        super
+      end
+
       def authorize_params
         super.tap do |params|
           params[:response_type] ||= DEFAULT_RESPONSE_TYPE
@@ -33,17 +37,31 @@ module OmniAuth
 
       info do
         {
+          'nickname' => raw_info['username'],
+          'email' => primary_email,
           'name' => raw_info['display_name'],
+          'image' => raw_info['links']['avatar']['href'],
         }
       end
 
       extra do
-        {:raw_info => raw_info}
+        {:raw_info => raw_info, :all_emails => emails}
       end
 
       def raw_info
         access_token.options[:mode] = :query
         @raw_info ||= access_token.get('user').parsed
+      end
+
+      def primary_email
+        primary = emails.find{ |i| i['is_primary'] && i['is_confirmed'] }
+        primary && primary['email'] || nil
+      end
+
+      def emails
+        access_token.options[:mode] = :query
+        email_response = access_token.get('user/emails').parsed
+        @emails ||= email_response && email_response['values'] || nil
       end
     end
   end
